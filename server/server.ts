22124d68d9ecc0d7e66b8b062c8daed1d68c2c9b
Thabish <kadertabish2@gmail.com>
2023-01-31 14:15:42 +0400

    server-intialized
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
 
 const app = express();
+const httpServer = createServer();
+const io = new Server(httpServer);
+
+io.on("connection", (socket: Socket) => {
+	console.log(socket.id);
+});
+
 app.listen(3000, () => {
 	console.log(`Server running ${3000}`);
 });
