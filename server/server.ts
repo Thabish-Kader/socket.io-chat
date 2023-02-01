79af9c10154868357861a59667f8b3b93ec21d47
Thabish <kadertabish2@gmail.com>
2023-02-01 11:40:25 +0400

    server
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Server, Socket } from "socket.io";
 import { createServer } from "http";
 import cors from "cors";
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 app.use(cors());
@@ -9,14 +10,26 @@ const server = createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:5173",
+		origin: ["http://localhost:5173", "https://admin.socket.io"],
 		methods: ["GET", "POST"],
+		credentials: true,
 	},
+});
+
+instrument(io, {
+	auth: false,
+	mode: "development",
 });
 
 io.on("connection", (socket: Socket) => {
 	socket.on("sendMsg", (data) => {
-		socket.broadcast.emit("getMsg", data);
+		console.log(data);
+		if (data.room === "") {
+			io.sockets.emit("getMsg", data.msg);
+		} else {
+			socket.join(data.room);
+			io.to(data.room).emit("getMsg", data.msg);
+		}
 	});
 });
 
